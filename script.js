720a29327f693695da26ce7ee467240e4956e451
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-06-19 20:33:32 -0400

    Add semicolon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   var userPick, compPick, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9;
-  var selectedCells = []
+  var selectedCells = [];
   var turn = 0;
 
   $('#option1, #option2').click(function() {
